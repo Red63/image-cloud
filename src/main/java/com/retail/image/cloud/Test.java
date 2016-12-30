8bb03305eb744c917f0c9628e849aa51cb30fc7c
red <luohong@retail-tek.com>
2016-12-30 18:35:23 +0800

    test

---
+++ src/main/java/com/retail/image/cloud/Test.java
@@ -0,0 +1,12 @@
+package com.retail.image.cloud;
+
+/**
+ * 描述:<br/>TODO; <br/>
+ * ClassName: com.retail.image.cloud.Test <br/>
+ * date: 2016-12-30 18:35 <br/>
+ *
+ * @author Red(luohong@retail-tek.com)
+ * @version 1.0.0
+ */
+public class Test {
+}
